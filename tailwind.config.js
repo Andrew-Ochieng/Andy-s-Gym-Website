8a48f44138418eba10797056d81b38de9858e9e7
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-09-13 01:43:52 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontWeight: {
+      hairline: 100,
+      thin: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+      black: 900,
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
